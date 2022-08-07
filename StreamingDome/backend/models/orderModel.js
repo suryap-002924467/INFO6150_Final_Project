fe18ef7b21a39fe1f475821daf7a16684f88efac
shereenjohn <john.sh@northeastern.edu>
2022-08-07 17:45:22 -0500

    Order Model Update

--- StreamingDome/backend/models/orderModel.js
+++ StreamingDome/backend/models/orderModel.js
@@ -1,7 +1,67 @@
 import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
-
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        original_title: { type: String, required: true },
+        qty: { type: Number, required: true },
+        poster_path: { type: String, required: true },
+          purchase: {type: String, required: false, default: "Buy"},
+          expiry_date: { type: String, required: false},
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Order = mongoose.model('Order', orderSchema)
