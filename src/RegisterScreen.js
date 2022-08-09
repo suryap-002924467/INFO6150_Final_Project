e28db49281667db1a40387f73622fd86b77963a4
sai supraja kosuri <98271852+suprajakosuri01@users.noreply.github.com>
2022-08-08 21:26:49 -0400

    Added validation for forms

--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { register } from '../actions/userActions'
+import { validEmail, validPassword ,validName} from '../RegEx';
 
 const RegisterScreen = ({ location, history }) => {
   const [name, setName] = useState('')
@@ -13,7 +14,10 @@ const RegisterScreen = ({ location, history }) => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState(null)
-
+  const [emailErr, setEmailErr] = useState('');
+  const [pwdError, setPwdError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [confirmPWdErr, setConfirmPasswordErr] = useState('');
   const dispatch = useDispatch()
 
   const userRegister = useSelector((state) => state.userRegister)
@@ -29,12 +33,65 @@ const RegisterScreen = ({ location, history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault()
+    setMessage("");
     if (password !== confirmPassword) {
       setMessage('Passwords do not match')
-    } else {
+    }
+      
+    console.log(name)
+    console.log(validName.test(name));
+
+
+    var letters = /^[A-Za-z]+$/;
+
+    setNameError("");
+
+    var correctName = true;
+    var correctPass = true;
+    var correctEmail = true;
+    var correctConfirmPass = true;
+
+    //name validation
+    if(!name.match(letters))
+     {
+      correctName = false;
+      setNameError('Invalid name');
+     }
+     //email validation
+
+     var emailValidation =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+     setEmailErr("");
+
+     if(!email.match(emailValidation)) {
+      correctEmail = false;
+      setEmailErr('Invalid email');
+     }
+
+     setPwdError("");
+     var passValidation = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,15}$/
+
+     if (!password.match(passValidation)) {
+      correctPass = false;
+      setPwdError('Invalid password')
+     }
+
+     setConfirmPasswordErr("");
+
+     if (!confirmPassword.match(passValidation)) {
+       correctConfirmPass = false;
+      setConfirmPasswordErr('Invalid confirm password')
+     }
+
+    if (name !== null && email !== null && password !== null && confirmPassword !== null 
+      && correctName && correctEmail && correctPass && correctConfirmPass)  {
+
+      console.log("Inside else block")
       dispatch(register(name, email, password))
     }
+  
   }
+  
 
   return (
     
@@ -45,6 +102,7 @@ const RegisterScreen = ({ location, history }) => {
       <h1 style={{color: "white"}} >Sign Up</h1>
       {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
+
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group   className="mb-3" controlId='name'>
@@ -54,7 +112,18 @@ const RegisterScreen = ({ location, history }) => {
             placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
-          ></Form.Control>
+          >
+
+          </Form.Control>
+          <div>
+         {/* {emailErr && <p>Your email is invalid</p>}
+         {pwdError && <p>Your password is invalid</p>} */}
+         {nameError}
+
+      </div>
+          <div>
+      </div>
+          
         </Form.Group>
 
         <Form.Group   className="mb-3" controlId='email'>
@@ -64,7 +133,14 @@ const RegisterScreen = ({ location, history }) => {
             placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
+          >
+            
+          </Form.Control>
+          <div>
+          {emailErr}
+         {/* {pwdError && <p>Your password is invalid</p>}
+         {nameError && <p>Your name is invalid</p>} */}
+      </div>
         </Form.Group>
 
         <Form.Group   className="mb-3" controlId='password'>
@@ -75,6 +151,11 @@ const RegisterScreen = ({ location, history }) => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
+          <div>
+         {/* {emailErr && <p>Your email is invalid</p>} */}
+         {pwdError}
+         {/* {nameError && <p>Your name is invalid</p>} */}
+      </div>
         </Form.Group>
 
         <Form.Group  className="mb-3" controlId='confirmPassword'>
@@ -85,11 +166,17 @@ const RegisterScreen = ({ location, history }) => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
+          <div>
+          {confirmPWdErr}
+          </div>
         </Form.Group>
 
         <Button type='submit' variant='primary'>
           Register
+         
         </Button>
+        
+        
       </Form>
 
       <Row className='py-3'>
