9e1e33f880b4382e810e4d474f6bd47872bdc802
suryap-002924467 <p.s@northeastern.edu>
2022-08-10 12:51:08 -0400

    Removed Delivery Field

--- StreamingDome/frontend/src/screens/ProfileScreen.js
+++ StreamingDome/frontend/src/screens/ProfileScreen.js
@@ -145,13 +145,13 @@ const ProfileScreen = ({ location, history }) => {
                       <i className='fas fa-times' style={{ color: 'red' }}></i>
                     )}
                   </td>
-                  <td>
+                  {/* <td>
                     {order.isDelivered ? (
                       order.deliveredAt.substring(0, 10)
                     ) : (
                       <i className='fas fa-times' style={{ color: 'red' }}></i>
                     )}
-                  </td>
+                  </td> */}
                   <td>
                     <LinkContainer to={`/order/${order._id}`}>
                       <Button className='btn-sm' variant='light'>
