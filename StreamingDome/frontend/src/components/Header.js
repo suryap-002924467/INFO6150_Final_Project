ddf3127d2161c2be1f47fdad9b880babdd42877a
ANJALI JAIN <98569940+JainAnja@users.noreply.github.com>
2022-08-10 16:14:41 -0400

    Update Header.js

--- StreamingDome/frontend/src/components/Header.js
+++ StreamingDome/frontend/src/components/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
                 </LinkContainer>
               )}
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
+                <NavDropdown title='Admin' id='adminmenu' style={{fontSize:"16px"}}>
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
